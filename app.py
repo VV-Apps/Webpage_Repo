9bce838bce4031b5bfffa7d347db837286f932a7
VV-Apps <136314524+VV-Apps@users.noreply.github.com>
2023-06-13 01:19:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
-
+from streamlit_option_menu import option_menu
 st.set_page_config(page_title="PGOS Website", page_icon=":music:",layout="wide")
 
-from streamlit_option_menu import option_menu
 
 # 1. as sidebar menu
 with st.sidebar:
