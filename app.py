75bf1b8bc496638e17ca141252ebd66ef4c7390c
VV-Apps <136314524+VV-Apps@users.noreply.github.com>
2023-06-14 01:04:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,27 +9,40 @@ st.sidebar.image("https://raw.githubusercontent.com/VV-Apps/Webpage_Repo/main/PG
 st.sidebar.title("Project Gift of Song")
 
 # Add tabs in a section next to the picture and title
-tabs = st.sidebar.button("About Us")
-if tabs:
-    st.title("Aims")
-    st.subheader("Vission & Mission")
-    st.write("Founded by a group of zealous youths with a passion for music, Project Gift of Song aims to bridge the gap between music and terminal illnesses. We wish to bring joy and comfort to all stages of life through the spread of music.")
+tabs1 = st.sidebar.button("About Us")
+tabs2 = st.sidebar.button("Campaigns & Activities")
+tabs3 = st.sidebar.button("Join Us")
 
-tabs = st.sidebar.button("Campaigns & Activities")
-if tabs:
-    st.title("Concerts")
-    st.subheader("Our Inaugural Music Concert")
-    st.write("Our Inaugural Music Concert to be held at the end of the year (3rd Dec)")
+# Create empty placeholders for tab contents
+about_us_placeholder = st.empty()
+campaigns_placeholder = st.empty()
+join_us_placeholder = st.empty()
 
-tabs = st.sidebar.button("Join Us")
-if tabs:
-    st.title("Structure")
+# Update tab content based on button press
+if tabs1:
+    about_us_placeholder.title("Aims")
+    about_us_placeholder.subheader("Vision & Mission")
+    about_us_placeholder.write("Founded by a group of zealous youths with a passion for music, Project Gift of Song aims to bridge the gap between music and terminal illnesses. We wish to bring joy and comfort to all stages of life through the spread of music.")
+else:
+    about_us_placeholder.empty()
+
+if tabs2:
+    campaigns_placeholder.title("Concerts")
+    campaigns_placeholder.subheader("Our Inaugural Music Concert")
+    campaigns_placeholder.write("Our Inaugural Music Concert to be held at the end of the year (3rd Dec)")
+else:
+    campaigns_placeholder.empty()
+
+if tabs3:
+    join_us_placeholder.title("Structure")
+else:
+    join_us_placeholder.empty()
 
 # Add other content in the main section
 with st.container():
-    if not any([st.session_state.get("About Us"), st.session_state.get("Campaigns & Activities"), st.session_state.get("Join Us")]):
+    if not any([tabs1, tabs2, tabs3]):
         st.title(":notes: Project Gift of Song")
-        st.subheader("A student run organisation founded in Singapore")
+        st.subheader("A student-run organization founded in Singapore")
         st.write("[Find out more about us 👈](https://tarannator.blogspot.com/)")
 
         st.write("---")
@@ -43,4 +56,3 @@ with st.container():
             )
 
 st.write("[Our Very Own YouTube Channel 👈](https://youtube.com)")
-
