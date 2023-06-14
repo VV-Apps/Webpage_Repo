0daaf361d324000d4354e3e253dbb47ea3fd3b78
VV-Apps <136314524+VV-Apps@users.noreply.github.com>
2023-06-14 15:35:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -73,19 +73,20 @@ with st.container():
                     st.image(gif_url, use_column_width=True)
                 st.write("[My YouTube Channel 👈](https://youtube.com)")
                 
-import base64
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('Dark Planet.jpg')    
+#import base64
+#def add_bg_from_local(image_file):
+#    with open(image_file, "rb") as image_file:
+#        encoded_string = base64.b64encode(image_file.read())
+#    st.markdown(
+#    f"""
+#    <style>
+#    .stApp {{
+#        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+#        background-size: cover
+#    }}
+#    </style>
+#    """,
+#    unsafe_allow_html=True
+#    )
+#add_bg_from_local('Dark Planet.jpg')    
+
