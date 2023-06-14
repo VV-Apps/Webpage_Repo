c6bf43d2251e4e34b148d516aaa30244c8b5f36a
VV-Apps <136314524+VV-Apps@users.noreply.github.com>
2023-06-14 13:03:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,52 +2,48 @@ import streamlit as st
 
 st.set_page_config(page_title="VV", page_icon=":wave:", layout="wide")
 
-# Display the picture from the repository in the sidebar
 st.sidebar.image("https://raw.githubusercontent.com/VV-Apps/Webpage_Repo/main/The Dark King.png", use_column_width=True)
-
-# Add the title "PGOS" beside the picture
 st.sidebar.title("VV_Webpage")
 
-# Add tabs in a section next to the picture and title
 tabs1 = st.sidebar.button("About Me")
 tabs2 = st.sidebar.button("Resume")
 tabs3 = st.sidebar.button("Contact Me")
 
-# Create empty placeholders for tab contents
-about_us_placeholder = st.empty()
+
+about_me_placeholder = st.empty()
 resume_placeholder = st.empty()
 contact_placeholder = st.empty()
 
-# Update tab content based on button press
-if tabs1:
-    about_us_placeholder.title("About Me")
-    about_us_placeholder.subheader("Vision")
-    about_us_placeholder.write("Founded by a group of zealous youths with a passion for music, Project Gift of Song aims to bridge the gap between music and terminal illnesses. We wish to bring joy and comfort to all stages of life through the spread of music.")
-    about_us_button = about_us_placeholder.button("Back to Main Page")
-    if about_us_button:
-        st.experimental_set_query_params()
-else:
-    about_us_placeholder.empty()
+with st.container():
+    if tabs1:
+        st.empty(title("About Me"))
+        about_me_placeholder.subheader("Vision")
+        about_me_placeholder.write("Founded by a group of zealous youths with a passion for music, Project Gift of Song aims to bridge the gap between music and terminal illnesses. We wish to bring joy and comfort to all stages of life through the spread of music.")
+        about_me_button = about_us_placeholder.button("Back to Main Page")
+        if about_me_button:
+            st.experimental_set_query_params()
+    else:
+        about_me_placeholder.empty()
+with st.container():
+    if tabs2:
+        resume_placeholder.title("Resume")
+        resume_placeholder.subheader("Education")
+        resume_placeholder.write("Educational details go here.")
+        resume_button = resume_placeholder.button("Back to Main Page")
+        if resume_button:
+            st.experimental_set_query_params()
+    else:
+        resume_placeholder.empty()
+with st.container():
+    if tabs3:
+        contact_placeholder.title("Contact Me")
+        contact_button = contact_placeholder.button("Back to Main Page")
+        if contact_button:
+            st.experimental_set_query_params()
+    else:
+        contact_placeholder.empty()
 
-if tabs2:
-    resume_placeholder.title("Resume")
-    resume_placeholder.subheader("Education")
-    resume_placeholder.write("Educational details go here.")
-    resume_button = resume_placeholder.button("Back to Main Page")
-    if resume_button:
-        st.experimental_set_query_params()
-else:
-    resume_placeholder.empty()
 
-if tabs3:
-    contact_placeholder.title("Contact Me")
-    contact_button = contact_placeholder.button("Back to Main Page")
-    if contact_button:
-        st.experimental_set_query_params()
-else:
-    contact_placeholder.empty()
-
-# Add other content in the main section
 with st.container():
     if not any([tabs1, tabs2, tabs3]):
         st.title(":grinning: VV")
