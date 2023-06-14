6e07057fd69b0df6af3f93b6b8930032069bfce8
VV-Apps <136314524+VV-Apps@users.noreply.github.com>
2023-06-14 14:55:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -72,3 +72,20 @@ with st.container():
                     gif_url = "https://raw.githubusercontent.com/VV-Apps/Webpage_Repo/main/The Dark King.gif"
                     st.image(gif_url, use_column_width=True)
                 st.write("[My YouTube Channel 👈](https://youtube.com)")
+                
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('The Dark King.png')    
